91b51335b33f96ea4e1afad1e390968dc63fd106
Lennart Doeppenschmitt <lennart@math.toronto.edu>
2016-07-06 18:40:32 -0400

    animation git add new.py

--- new.py
+++ new.py
@@ -2,13 +2,25 @@ import sys
 import time
 from numpy import *
 from pylab import *
+from matplotlib import animation
 
 N=200
 dt=0.01
 dx=0.001
-x=arange(-10,10,dx)
+x=arange(-8,8,dx)
 hbar=1.
 m=1.
+DATA = []
+
+
+def init():
+    line.set_data([], [])
+    return line,
+
+def animate(i):
+    y = DATA[i]
+    line.set_data(x, y)
+    return line,
 
 def V(x):
     if x**2 > 100.:
@@ -19,8 +31,9 @@ def V(x):
 
 def nonlinearStep( data_init,x, V_array, dt):
     data1 = data_init+V_array*data_init*dt/(1.j*hbar)
-    
     return data1
+
+
 def fourierStep(data_init,x,dt):
     data1 = fft(data_init)
     data2 = fftshift(data1)
@@ -36,7 +49,6 @@ def fourierStep(data_init,x,dt):
 if __name__ == "__main__":
     runtime = time.time()
 
-    DATA = []
     V_array = map(V,x)
 
 
@@ -52,5 +64,9 @@ if __name__ == "__main__":
         DATA.append(f_ini)
             
     print "\n\n" + "program done in " + str(time.time() -runtime) + "\n\n"
-    plot(x,DATA[180])
+    fig = figure()
+    ax = axes(xlim=(-8, 8), ylim=(-1, 2))
+    line, = plot([], [], lw=2)
+
+    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=200, interval=80, blit=True)
     show()
